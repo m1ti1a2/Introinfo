f53ebe8b7008b452a582c1f86ff58ef7008b2d6c
matias <msaq2005@gmail.com>
2023-03-28 15:04:50 -0600

    n

---
+++ practica.py
@@ -0,0 +1,43 @@
+# ejercicio 1 hacer una cadena de hola mundo
+programa = "hola mundo"
+
+# ejercicio 2 escribir una variable que diga hola mundo y que lo muestre
+programa = "hola mundo"
+print (programa)
+
+# ejercicio 3 hacer un nombre de usuario para el programa
+
+nombre = "matias"
+print (nombre)
+
+# ejercicio 4 operacion aritmetica
+
+n1 = 11
+n2 = 15 
+
+result = n1 + n2
+print (result)
+
+# ejercicio 5 mostrar el numero de horas de trabajo y cuanto es la suma del cobro
+
+almacene_el_numero_horas_y_coste_de_horas= 8 + 12
+
+result =  almacene_el_numero_horas_y_coste_de_horas
+print(result)
+
+# ejercicio 6 masa corporal
+
+kg= 8
+m = 185
+tu_indice_de_masa_corporal_es_de = kg / m
+print(tu_indice_de_masa_corporal_es_de)
+
+
+# ejercicio 7 matematicas
+n= 17
+m2= 15
+c= n+m2
+r= 42
+
+result_2= c - r
+print(result_2)
