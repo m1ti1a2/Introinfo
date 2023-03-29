2fae9c301b92d34342966cf0f8cb2a1107856764
matias <msaq2005@gmail.com>
2023-03-28 20:34:26 -0600

    n

---
+++ bubles.py
@@ -0,0 +1,19 @@
+# # # un bucle while repite su bucle de codigo mientras su rol es True
+# # # crear una variable que detenga el bucle llamado control
+
+control = True
+while control == True:
+    print("hola mundo")
+
+    control = False
+
+    print("se mostro una vez")
+
+# Hacer el contador
+
+contador = 1 
+
+while contador < 10:
+    print(contador)
+    contador += 1 
+
---
+++ practica_de_trabajo.py
@@ -0,0 +1,47 @@
+n1 = 12
+n2 = 14
+
+suma = n1 + n2
+
+print(suma)
+
+# resta
+
+n3= 15
+n4= 7
+
+resta= n3-n4
+
+print(resta)
+
+# multiplicacion
+
+n5= 2
+n6= 2
+
+multi = n5 * n6
+
+print(multi)
+
+# division
+
+numero_1= 6
+numero_2= 3
+
+division= numero_1 / numero_2
+
+print(division)
+
+
+# circunferencia 
+area= 3,14 * 25
+print(area) 
+longitud= 2 * 3,14 * 5 
+print(longitud)
+
+
+# numero par o impar
+number = 5
+number_2 = 4
+
+
